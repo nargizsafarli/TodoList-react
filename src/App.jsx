ccbd8c7a506879c1123eb873d1c9a2ce5b7688f6
Nergiz Seferli <nergizsafarli60@gmail.com>
2025-02-22 18:25:23 +0400

    Added to localeStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,48 @@
 
-import React, { useState }   from 'react'
-import TodoForm from './Components/TodoForm/TodoForm'
-import TodoList from './Components/TodoList/TodoList'
-import "./App.css"
-import SearchTodo from './Components/Search/SearchTodo'
+import React, { useState, useEffect } from 'react';
+import TodoForm from './Components/TodoForm/TodoForm';
+import TodoList from './Components/TodoList/TodoList';
+import "./App.css";
+import SearchTodo from './Components/Search/SearchTodo';
 
 function App() {
-  
-  const [todo,setTodo]=useState([])
-  console.log(todo);
-
-  const createTodo=(newTodo)=>{
-    setTodo([...todo,newTodo])
-  }
+  const [todo, setTodo] = useState([]);
   const [searchText, setSearchText] = useState("");
 
-  const deleteTodo=(id)=>{
-    setTodo(todo.filter((el)=>el.id!==id)) 
-  }
+
+  useEffect(() => {
+   
+      const savedTodos = JSON.parse(localStorage.getItem("todos"));
+       setTodo(savedTodos)
+  }, []);
+
+  const createTodo = (newTodo) => {
+    const updatedTodos = [...todo, newTodo];
+    setTodo(updatedTodos);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos))
+  };
+
+  const deleteTodo = (id) => {
+    const updatedTodos = todo.filter((el) => el.id !== id);
+    setTodo(updatedTodos);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos))
+  };
 
   const updateTodo = (id, newText) => {
-    setTodo(todo.map((el) => (el.id === id ? { ...el, text: newText} :el)));
+    const updatedTodos = todo.map((el) =>
+      el.id === id ? { ...el, text: newText } : el
+    );
+    setTodo(updatedTodos);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos)); 
   };
 
   return (
     <div className='main-container'>
-      <TodoForm createTodo={createTodo}/>
-      <SearchTodo searchText={searchText} setSearchText={setSearchText}/>
-      <TodoList todo={todo} searchText={searchText} deleteTodo={deleteTodo} updateTodo={updateTodo}/>
-     
-     
+      <TodoForm createTodo={createTodo} />
+      <SearchTodo searchText={searchText} setSearchText={setSearchText} />
+      <TodoList todo={todo} searchText={searchText} deleteTodo={deleteTodo} updateTodo={updateTodo} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
